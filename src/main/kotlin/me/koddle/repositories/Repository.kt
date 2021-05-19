31e2f96cab32e1f1fda27320240088d6a7e21d8d
Colin Childs <colinchilds@gmail.com>
2021-05-18 21:25:15 -0600

    Inject DB schema

--- src/main/kotlin/me/koddle/repositories/Repository.kt
+++ src/main/kotlin/me/koddle/repositories/Repository.kt
@@ -12,38 +12,36 @@ import me.koddle.json.jArr
 import me.koddle.json.jObj
 import me.koddle.tools.DatabaseAccess
 import org.koin.core.KoinComponent
-import org.koin.core.inject
+import org.koin.core.get
 
 
-abstract class Repository(val table: String, val schema: String) : KoinComponent {
+abstract class Repository(val table: String) : KoinComponent {
 
-    private val da: DatabaseAccess by inject()
-
-    val tableName = "$schema.$table"
+    private val da: DatabaseAccess = get()
 
     suspend fun all(connection: SqlClient? = null): JsonArray {
-        return query("SELECT * FROM $tableName", conn = connection)
+        return query("SELECT * FROM $table", conn = connection)
     }
 
     suspend fun find(id: String, connection: SqlClient? = null): JsonObject {
-        val result = queryOne("SELECT * FROM $tableName WHERE id = $1", Tuple.of(id), connection)
+        val result = queryOne("SELECT * FROM $table WHERE id = $1", Tuple.of(id), connection)
         if (result.isEmpty)
             throw ModelNotFoundException("No object found with ID", jArr(id))
         return result
     }
 
     suspend fun insert(data: JsonObject, connection: SqlClient? = null): JsonObject {
-        val query = "INSERT INTO $tableName (data) VALUES ($1::jsonb) RETURNING *"
+        val query = "INSERT INTO $table (data) VALUES ($1::jsonb) RETURNING *"
         return queryOne(query, Tuple.of(data), connection)
     }
 
     suspend fun update(id: String, data: JsonObject, connection: SqlClient? = null): JsonObject {
-        val query = "UPDATE $tableName SET data = $1 WHERE id = $2 RETURNING *"
+        val query = "UPDATE $table SET data = $1 WHERE id = $2 RETURNING *"
         return queryOne(query, Tuple.of(data, id), connection)
     }
 
     suspend fun delete(id: String, connection: SqlClient? = null): JsonObject {
-        val query = "DELETE FROM $tableName WHERE id = $1 RETURNING id"
+        val query = "DELETE FROM $table WHERE id = $1 RETURNING id"
         val deleted = queryOne(query, Tuple.of(id), connection)
         if (deleted.isEmpty)
             throw ModelNotFoundException("Tried to delete an item that does not exist", jArr(id))
--- src/main/kotlin/me/koddle/tools/DatabaseAccess.kt
+++ src/main/kotlin/me/koddle/tools/DatabaseAccess.kt
@@ -12,14 +12,14 @@ import me.koddle.exceptions.ModelNotFoundException
 class DatabaseAccess {
     val pool: PgPool
 
-    constructor(config: JsonObject, vertx: Vertx) {
+    constructor(config: JsonObject, vertx: Vertx, schema: String = "public") {
         val dbName = config.getString("SERVICE_DB_NAME")
         val connectOptions = pgConnectOptionsOf(port = config.getInteger("SERVICE_DB_PORT"),
             host = config.getString("SERVICE_DB_HOST"),
             database = dbName,
             user = config.getString("SERVICE_DB_USER"),
             password = config.getString("SERVICE_DB_PASSWORD"),
-            properties = mapOf("search_path" to config.getString("schema", "public")))
+            properties = mapOf("search_path" to schema))
         val poolOptions = poolOptionsOf(maxSize = config.getInteger("SERVICE_DB_MAX_SIZE"),
             maxWaitQueueSize = config.getInteger("SERVICE_DB_MAX_QUEUE_SIZE"))
         pool = PgPool.pool(vertx, connectOptions, poolOptions)
